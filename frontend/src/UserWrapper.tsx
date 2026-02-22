39a78b85fe62d7a69a97e13581e98298a60c0e2c
Rourke Smith <smithrourke@gmail.com>
2026-02-22 18:35:51 -0500

    refactor: remove ConnectionBanner from UserWrapper - now in socket providers

--- frontend/src/UserWrapper.tsx
+++ frontend/src/UserWrapper.tsx
@@ -1,13 +1,11 @@
 import { UserProvider } from "./userProvider";
 import { RouteSectionProps } from "@solidjs/router";
 import GlobalNavBar from "./components/GlobalNavBar";
-import ConnectionBanner from "./ConnectionBanner";
 
 export const UserWrapper = (props: RouteSectionProps) => {
     return (
         <UserProvider>
             <div class="flex h-screen flex-col">
-                <ConnectionBanner />
                 <GlobalNavBar />
                 <div class="flex flex-1 overflow-hidden">{props.children}</div>
             </div>
